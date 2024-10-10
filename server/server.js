449389f5e443e65af65d4681fcfeb91176b49f51
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-10-10 18:29:20 -0500

    Fixed the server.js to serve the dist folder instead of build for Vite! now properly rendering the frontend of the site

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,6 +20,14 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
 
 db.once('open', () => {
   app.listen(PORT, () => {
